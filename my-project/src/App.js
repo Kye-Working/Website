514947f0266bf8437871e3616740da395df5da45
Kye-Working <kyefworkin@gmail.com>
2023-10-27 02:02:57 +0100

    Navbar (Skeleton)
    - Basic navbar

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Navbar from "./Navbar.jsx";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  );
 }
 
 export default App;
---
+++ my-project/src/Navbar.jsx
@@ -0,0 +1,28 @@
+function Navbar() {
+  return (
+    <nav className=" flex justify-between">
+      <div className="">
+        <a href="#" className="">
+          @Kye
+        </a>
+      </div>
+      <div className=" space-x-5">
+        <a href="#" className="">
+          Profile
+        </a>
+        <a href="#" className="">
+          Projects
+        </a>
+        <a href="#" className="">
+          Experience
+        </a>
+        <a href="#" className="">
+          Contact
+        </a>
+      </div>
+      <div></div>
+    </nav>
+  );
+}
+
+export default Navbar;
